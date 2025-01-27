11e97c689460c7fcce823c91853e4dcc69f788e8
Renee Rudderow <rrudderow@yahoo.com>
2025-01-27 09:05:34 -0700

    struct

---
+++ simple/simple1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+struct Data {
+    double a;
+    long b;
+};
+
+int main() {
+    cout << "Hello CSCI 460" << endl;
+    cout << sizeof(Data) << endl;
+    return 0;
+}
