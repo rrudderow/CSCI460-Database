559a3c63c5af92c616fcccdf49e0c0f89f332208
Renee Rudderow <rrudderow@yahoo.com>
2025-01-29 09:48:12 -0700

    ostream

--- simple/simple1.cpp
+++ simple/simple1.cpp
@@ -9,17 +9,18 @@ ostream & operator <<(ostream &out, bool &b){
     else return out << "False";
 }*/
 
-class Data {
+class __attribute__ ((packed)) Data {
     public:
     int b;
     double a;
     bool c;
     void write(ostream &out, long pos){
-        //seekp = seek to put at position(in bytes)
+        //seekp = seek to put some stuff at position(in bytes)
         out.seekp(pos*sizeof(Data));
         out.write((char *)this, sizeof(Data));
     }
     void read(ifstream &in, long pos){
+        //seekg = seek to get some stuff from given position(in bytes)
         in.seekg(pos*sizeof(Data));
         in.read((char *)this, sizeof(Data));
     }
@@ -40,9 +41,17 @@ int main() {
     ifstream fin;
     fout.open("Data.bin");
     d.write(fout,0);
+    d.b+=2;
+    d.a+=2;
+    d.c=true;
+
+    d.write(fout,4);
+    //second parameters of write() is block - 
+        //must read out same block you wrote to to get same value
+
     fout.close();
     fin.open("Data.bin");
-    e.read(fin,0);
+    e.read(fin,4);
     cout << e << endl;
     fin.close();
     return 0;
