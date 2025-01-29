94d563a6fe4a4bac3a6306c2a89cd4616954cbaa
Renee Rudderow <rrudderow@yahoo.com>
2025-01-29 09:15:29 -0700

    ostream

--- simple/simple1.cpp
+++ simple/simple1.cpp
@@ -8,15 +8,31 @@ class Data {
     public:
     long b;
     double a;
+    void write(ostream &out, long pos){
+        //seekp = seek to put at position
+        out.seekp(pos);
+        out.write((char *)this, sizeof(Data));
+    }
+    void read(ifstream &in, long pos){
+
+    }
+    friend ostream & operator <<(ostream &out, Data &item){
+        return out << "a: " << item.a << ", b: " << item.b << endl;
+    }
 };
 
 int main() {
-    Data d;
+    Data d, e;
     d.a=-1.0;
     d.b=-1;
     ofstream fout;
+    ifstream fin;
     fout.open("Data.bin");
-    fout.write((char *)&d,sizeof(Data));
+    d.write(fout,0);
     fout.close();
+    fin.open("Data.bin");
+    e.read(fin,0);
+    cout << e << endl;
+    fin.close();
     return 0;
 }
