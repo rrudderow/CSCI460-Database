e66c2fb11535d98aa08fee3980eab082a77a8492
Renee Rudderow <rrudderow@yahoo.com>
2025-02-03 09:41:12 -0700

    compiles

--- simple/simple1.cpp
+++ simple/simple1.cpp
@@ -9,33 +9,61 @@ ostream & operator <<(ostream &out, bool &b){
     else return out << "False";
 }*/
 
-class __attribute__ ((packed)) Data {
+class __attribute__ ((packed)) DataItems{
     public:
     int b;
     double a;
     bool c;
+};
 
-    long getSize(istream &in){
+const int MAXBYTES=64;
+class __attribute__ ((packed)) Data {
+    //attributes(packed) makes it so only use as many bytes as you need - instead of 8 bytes for everything
+    public:
+    DataItems data;
+    bool used;
+    char gap[MAXBYTES-sizeof(DataItems)-1];
+
+    static long getSize(istream &in){
         in.seekg(0,ios_base::end);
         return in.tellg()/sizeof(Data);
+        //bc static use Data::getSize() instead of d.getSize() - use Data as this pointer
     }
-    void write(ostream &out, long pos){
+    void write(ostream &out, long pos, bool setUsed=true){
         //seekp = seek to put some stuff at position(in bytes)
         out.seekp(pos*sizeof(Data));
+        if(setUsed) used=true;
         out.write(reinterpret_cast<char *>(this), sizeof(Data));
         out.flush(); //makes computer write to file immediately - instead of waiting
         //otherwise won't flush until after newline char so need space between write and read
     }
-    void read(ifstream &in, long pos){
+    void read(istream &in, long pos){
         //seekg = seek to get some stuff from given position(in bytes)
         in.seekg(pos*sizeof(Data));
         in.read(reinterpret_cast<char *>(this), sizeof(Data));
     }
     friend ostream & operator <<(ostream &out, Data &item){
-        out << "a: " << item.a << ", b: " << item.b << ", c: ";
-        if(item.c) out << "True" << endl;
+        out << "a: " << item.data.a << ", b: " << item.data.b << ", c: ";
+        if(item.data.c) out << "True" << endl;
         else out << "False" << endl;
         return out;
+    }
+
+    long dataNew(istream &in){
+        long n=getSize(in);
+        for(long i=0;i<n;i++){
+            Data d;
+            d.read(in,i);
+            if(d.used==false) return i;
+        }
+        return n;
+    }
+
+    void dataDelete(ostream &out,long pos){
+        //doesn't actually delete - just marks space as avaliable for something else
+        Data d;
+        d.used=false;
+        d.write(out,pos,false);
     }
 };
 
@@ -47,9 +75,9 @@ int main() {
     fout.open("Data.bin");
     fin.open("Data.bin");
 
-    d.a=-1.0;
-    d.b=-1;
-    d.c=true;
+    d.data.a=-1.0;
+    d.data.b=-1;
+    d.data.c=true;
 
     d.write(fout,0);
     //need a flush here to make sure data makes it to disk before you read
@@ -57,15 +85,15 @@ int main() {
     e.read(fin,0);
     cout << e;
 
-    d.b+=2;
-    d.a+=2;
-    d.c=true;
+    d.data.b+=2;
+    d.data.a+=2;
+    d.data.c=true;
 
-    cout << d.getSize(fin) << endl;
+    cout << Data::getSize(fin) << endl;
     d.write(fout,4);
     //second parameters of write() is block - 
         //must read out same block you wrote to to get same value
-    cout << d.getSize(fin) << endl;
+    cout << Data::getSize(fin) << endl;
 
     fout.close();
     fin.close();
