1af0811ae9ac0363ee6e7e6d39d129bd17777f93
Renee Rudderow <rrudderow@yahoo.com>
2025-02-05 09:49:22 -0700

    notes

--- simple/simple1.cpp
+++ simple/simple1.cpp
@@ -14,9 +14,19 @@ class __attribute__ ((packed)) DataItems{
     int b;
     double a;
     bool c;
+    // char first[20];
+    // char last[20];
+    // char address[100];
+    friend ostream & operator <<(ostream &out, Data &item){
+        out << "a: " << item.a << ", b: " << item.b << ", c: ";
+        if(item.c) out << "True" << endl;
+        else out << "False" << endl;
+        return out;
+    }
 };
 
 const int MAXBYTES=32;
+const long FILENULL=0;
 class __attribute__ ((packed)) Data {
     //attributes(packed) makes it so only use as many bytes as you need - instead of 8 bytes for everything
     public:
@@ -26,14 +36,33 @@ class __attribute__ ((packed)) Data {
     //class = struct with members that are functions
         DataItems data;
         long next;
+        //null pointer = 0 and can never access
     }
     bool used;
+    //true if memory block is being used fro data
+    //use to determine if using dataitems or the next
+    //look at it as only one of the two
+
+    //assumes size of dataitems is larger than long
     char gap[MAXBYTES-sizeof(DataItems)-1];
+    //maxbytes minus size of union minus one for used bool
+    //min and max are macros
+
+
+    friend ostream & operator <<(ostream &out, Data &item){
+        if(item.used)
+            out << "Data Node: " << item.dataList.data << endl;
+        else
+            out << "Link List Node: " << item.dataList.next << endl;
+        return out;
+    }
 
     static long getSize(istream &in){
+        //bc static use Data::getSize() instead of d.getSize() - use class name as this pointer
         in.seekg(0,ios_base::end);
-        return in.tellg()/sizeof(Data);
-        //bc static use Data::getSize() instead of d.getSize() - use Data as this pointer
+        return in.tellg()/sizeof(Data)-1;
+        //subtract one bc first memory location for linked list
+
     }
     void write(ostream &out, long pos, bool setUsed=true){
         //seekp = seek to put some stuff at position(in bytes)
@@ -43,20 +72,27 @@ class __attribute__ ((packed)) Data {
         out.flush(); //makes computer write to file immediately - instead of waiting
         //otherwise won't flush until after newline char so need space between write and read
     }
+
     void read(istream &in, long pos){
         //seekg = seek to get some stuff from given position(in bytes)
         in.seekg(pos*sizeof(Data));
         in.read(reinterpret_cast<char *>(this), sizeof(Data));
     }
-    friend ostream & operator <<(ostream &out, Data &item){
-        out << "a: " << item.data.a << ", b: " << item.data.b << ", c: ";
-        if(item.data.c) out << "True" << endl;
-        else out << "False" << endl;
-        return out;
+
+    static void createDB(string fname){
+        //file location zero should not be used for data - just for linked list
+        Data d;
+        d.used=false;
+        d.dataList.next=FILENULL;
+        ofstream fout;
+        fout.open(fname);
+        write(fout,FILENULL,false);
+        fout.close();
     }
 
     static long dataNew(istream &in){
         long n=getSize(in);
+        //this for loop makes On, but if use linked list would be constant time
         for(long i=0;i<n;i++){
             Data d;
             d.read(in,i);
@@ -70,6 +106,30 @@ class __attribute__ ((packed)) Data {
         Data d;
         d.used=false;
         d.write(out,pos,false);
+    }
+};
+
+class Database{
+    public:
+    fstream file;
+    static void createDB(string fname){
+        Data::createDB(fname);
+    }
+    //constructor should open database file and keep the file handle for us
+    Database(string fname){
+        file.open(fname);
+    }
+    void write(long pos,Data d){
+        d.write(file,pos,true);
+    }
+    Data read(long pos){
+        Data d;
+        d.read(file,pos);
+        return d;
+    }
+    ~Database(){
+        file.flush();
+        file.close();
     }
 };
 
