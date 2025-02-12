a38d9c0c6e9d96e7a13bc4d27a792c1e8fbd3e77
Renee Rudderow <rrudderow@yahoo.com>
2025-02-12 09:50:21 -0700

    hello world wensite

--- app/index.js
+++ app/index.js
@@ -1,10 +1,12 @@
+// This is back end code - code run on the server
 const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/index.html', (req, res) => {
   const options = {
+    //attributes: values - json notation
     root: path.join(__dirname)
   }
   res.sendFile('index.html',options)
@@ -13,5 +15,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-  console.log(`Running on website http://localhost:${port}/`)
+  console.log(`Running on website http://localhost:${port}/index.html`)
 })
---
+++ app/script.js
@@ -0,0 +1,1 @@
+// This is front end code - code run in the browser
