d02d8bb3f38314eba7f166e7047a15f698a2a14d
Renee Rudderow <rrudderow@yahoo.com>
2025-02-17 09:49:53 -0700

    index.js

--- app/index.js
+++ app/index.js
@@ -3,14 +3,41 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
+const options = {
+  //attributes: values - json notation
+  root: path.join(__dirname)
+}
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'csci460', //don't do this in real life
+  host: 'localhost',
+  database: 'csci460',
+  password: '206Rr153',
+})
+
+pool.query('SELECT id,first,last,salary FROM first1', (error, results) => {
+  if(error) {
+    throw error
+  }
+  console.log(results.rows)
+})
 
 app.get('/index.html', (req, res) => {
-  const options = {
-    //attributes: values - json notation
-    root: path.join(__dirname)
-  }
   res.sendFile('index.html',options)
   //console.log(req)
+})
+
+app.get('/styles.css',(req, res) => {
+  res.sendFile('styles.css',options)
+})
+
+app.get('/script.js',(req, res) => {
+  res.sendFile('script.js',options)
+})
+
+app.get('/getData',(req, res) => {
+  res.send()
 })
 
 app.listen(port, () => {
--- app/script.js
+++ app/script.js
@@ -1,1 +1,46 @@
-// This is front end code - code run in the browser
+/* This is Front-End Code */
+/* Code run in the browser */
+
+function getData(tag) {
+    fetch("/getData").then((response)=>{
+      //fetch is a network delay - does file exist, can I get file
+      response.json().then((obj)=>{
+        obj.forEach((d)=>{
+          tag.innerHTML+=`<tr><td>${d.id}</td><td>${d.first}</td><td>${d.last}</td><td>${d.salary}</td><td>`
+        })
+      })
+    })
+  }
+
+  /*
+async function getData() {
+    //let dbData=[{id:2,first:"Bob",last:"Smith",salary:20000},
+    //  {id:1,first:"Karl",last:"Castleton",salary:10000}
+    //];
+    const url = "/getData";
+    let dbData={};
+    let returnValue='';
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const json = await response.text();
+      console.log(json);
+      dbData=JSON.parse(json);
+      dbData.forEach((data)=>{
+          returnValue+=`<tr><td>${data.id}</td><td>${data.first}</td><td>${data.last}</td><td>${data.salary}</td></tr>`;
+        });
+      return returnValue;
+      } catch (error) {
+      console.error(error.message);
+    }
+  }*/
+  
+  async function doLoad() {
+      let tag=document.getElementById('salaryTable');
+      if (tag) getData(tag)
+  }
+  
+  window.addEventListener('load',doLoad);
+  
